d9ad7cff77be13278466fcbf5997e2f40bf48b16
HannaKaniuk <136392947+HannaKaniuk@users.noreply.github.com>
2023-11-09 14:55:36 +0200

    homework11

--- src/index.js
+++ src/index.js
@@ -5,29 +5,38 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 const apiKey = '40538294-532b9d41dacfc837c400cb4b1';
 const perPage = 40;
 let page = 1;
+let loading = false; 
 
 const refs = {
   searchForm: document.getElementById('search-form'),
   gallery: document.getElementById('gallery'),
-  loadMoreButton: document.getElementById('load-more'),
 };
 
 refs.searchForm.addEventListener('submit', function (e) {
   e.preventDefault();
   page = 1;
-  refs.gallery.innerHTML = ''; // Обновляем использование refs
+  refs.gallery.innerHTML = '';
   loadImages();
 });
 
-refs.loadMoreButton.addEventListener('click', function () {
-  page++;
-  loadImages();
+
+window.addEventListener('scroll', function () {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+
+  if (scrollTop + clientHeight >= scrollHeight - 100 && !loading) {
+    page++;
+    loadImages();
+  }
 });
+
+let hasShownTotalHitsMessage = false;
 
 async function loadImages() {
   const searchQuery = document.querySelector('input[name="searchQuery"]').value;
 
   try {
+    loading = true;
+
     const response = await axios.get('https://pixabay.com/api/', {
       params: {
         key: apiKey,
@@ -41,15 +50,16 @@ async function loadImages() {
     });
 
     const images = response.data.hits;
+    const totalHits = response.data.totalHits;
 
     if (images.length === 0) {
       alert('No images found for your search.');
     } else {
       displayImages(images);
-    }
-
-    if (images.length < perPage) {
-      refs.loadMoreButton.style.display = 'none';
+      if (!hasShownTotalHitsMessage) {
+        showTotalHitsMessage(totalHits);
+        hasShownTotalHitsMessage = true;
+      }
     }
 
     const lightbox = new SimpleLightbox('.photo-card a', {
@@ -58,7 +68,25 @@ async function loadImages() {
     lightbox.refresh();
   } catch (error) {
     console.error('Error fetching images:', error);
+  } finally {
+    loading = false;
   }
+}
+
+function showTotalHitsMessage(totalHits) {
+  const messageContainer = document.createElement('div');
+  messageContainer.textContent = `Hooray! We found ${totalHits} images.`;
+  document.body.appendChild(messageContainer);
+
+  
+  setTimeout(() => {
+    document.body.removeChild(messageContainer);
+  }, 5000); 
+}
+
+
+function showTotalHitsMessage(totalHits) {
+  alert(`Hooray! We found ${totalHits} images.`);
 }
 
 function displayImages(images) {
@@ -71,16 +99,16 @@ function displayImages(images) {
         </a>
         <div class="info">
           <p class="info-item">
-            <b>Лайки</b> ${image.likes}
+            <b>Likes</b> ${image.likes}
           </p>
           <p class="info-item">
-            <b>Просмотры</b> ${image.views}
+            <b>Views</b> ${image.views}
           </p>
           <p class="info-item">
-            <b>Комментарии</b> ${image.comments}
+            <b>Comments</b> ${image.comments}
           </p>
           <p class="info-item">
-            <b>Загрузки</b> ${image.downloads}
+            <b>Downloads</b> ${image.downloads}
           </p>
         </div>
       </div>
@@ -90,6 +118,3 @@ function displayImages(images) {
 }
 
 
-const lightbox = new SimpleLightbox('.photo-card a', {
-  animationSpeed: 100,
-});
